2de9b08f8bb8ccffcc9686d910ddaf5dde8c486b
RJ Luengas <robertojla8@gmail.com>
2023-12-10 22:24:49 -0600

    Se agregaron las tipografías: Pacifico y SourceSans. Se completo el Widget main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 backgroundImage: AssetImage('images/rj.jpeg'),
@@ -22,11 +23,65 @@ class MyApp extends StatelessWidget {
               Text(
                 "RJ",
                 style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Pacifico"),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                "Full Stack Mobile && Web",
+                style: TextStyle(
                   color: Colors.white,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "SourceSans",
+                  letterSpacing: 2.5,
                 ),
-              )
+              ),
+              SizedBox(
+                height: 30.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white70,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 15.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "56 50 81 20 96",
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        fontFamily: "SourceSans",
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 15.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "robertojla8@gmail.com",
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        fontFamily: "SourceSans",
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
